36baf44d510b0298c6cc21e497ecb87664c06b79
louisnesta <63044322+louisnesta@users.noreply.github.com>
2020-04-15 10:18:16 +0100

    Update Hero.vue

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -24,7 +24,7 @@ export default {
       if (this.custom.background.url) {
         return {
           background: `url('${this.custom.background.url}')`,
-          backgroundPosition: "0 -500px",
+          backgroundPosition: "0",
           backgroundSize: "cover"
         }
       } else if (this.custom.background.color) {
